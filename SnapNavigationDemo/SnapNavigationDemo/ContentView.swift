1fde1fb670fb28b60c39430f4ec8622f8575349f
Simon Nickel <simonnickel@googlemail.com>
2024-08-16 12:43:19 +0200

    Add demo project

---
+++ SnapNavigationDemo/SnapNavigationDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnapNavigationDemo
+//
+//  Created by Simon Nickel on 16.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnapNavigationDemo/SnapNavigationDemo/SnapNavigationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapNavigationDemoApp.swift
+//  SnapNavigationDemo
+//
+//  Created by Simon Nickel on 16.08.24.
+//
+
+import SwiftUI
+
+@main
+struct SnapNavigationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
