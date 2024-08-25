6de095c4b037a8efe9e5ab7b00c72d2e18a51e9c
Simon Nickel <simon.nickel@chrono24.com>
2024-08-25 20:44:37 +0200

    Implement SnapNavigationSplitView using NavState

--- Sources/SnapNavigation/View/SnapNavigationSplitView.swift
+++ Sources/SnapNavigation/View/SnapNavigationSplitView.swift
@@ -15,21 +15,28 @@ public struct SnapNavigationSplitView<Item: SnapNavigationItem>: View {
         self.state = state
     }
 
+    /// SnapNavigationSplitView would benefit of NavState being @Observable.
+    /// But SnapNavigationTabView does not work properly when NavState is @Observable (navigation animations no longer appear).
+    /// So as a workaround the selection state is handled here and forwarded to the State.
+    @State private var selected: Item? {
+        didSet {
+            state.selectedBinding.wrappedValue = selected
+        }
+    }
+
     public var body: some View {
         NavigationSplitView {
-            List(state.items, id: \.self, selection: state.selectedBinding) { item in
+            List(state.items, id: \.self, selection: $selected) { item in
                 AnyView(item.label)
             }
         } detail: {
-            NavigationStack {
-                if let selection = state.selected {
-                    SnapNavigationDestinationScreen(item: selection)
-                        .navigationDestination(for: Item.self) { item in
-                            SnapNavigationDestinationScreen(item: item)
-                        }
-                } else {
-                    EmptyView()
-                }
+            if let item = selected {
+                SnapNavigationStack(
+                    path: state.pathBinding(for: item),
+                    root: item
+                )
+            } else {
+                EmptyView() // TODO:
             }
         }
     }
