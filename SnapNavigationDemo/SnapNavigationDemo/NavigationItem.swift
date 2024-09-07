9d96a864394c2d9bb1d44a72ed2a49e65aac6b79
Simon Nickel <simonnickel@googlemail.com>
2024-09-07 21:10:03 +0200

    Fix circle root

--- SnapNavigationDemo/SnapNavigationDemo/NavigationItem.swift
+++ SnapNavigationDemo/SnapNavigationDemo/NavigationItem.swift
@@ -22,11 +22,11 @@ enum NavigationItem: SnapNavigationItem {
 				
 			case .rectangleItem(level: let level): .init(title: "Rectangle \(level)", systemImage: "\(level).rectangle")
 				
-            case .circle: .init(title: "Circle", systemImage: "circle") { item in
+            case .circle: .init(title: "Circle", systemImage: "circle")
+				
+			case .circleSection: .init(title: "Circles", systemImage: "circle") { item in
 				ItemList(item: item)
 			}
-				
-			case .circleSection: .init(title: "Circles", systemImage: "circle")
 				
 			case .circleItem(level: let level): .init(title: "Circle \(level)", systemImage: "\(level).circle")
             
