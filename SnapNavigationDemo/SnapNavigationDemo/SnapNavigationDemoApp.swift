0424a58d5e9583401772b9cd4fafc191409a4041
Simon Nickel <simonnickel@googlemail.com>
2024-11-21 20:20:45 +0100

    Cleanup

--- SnapNavigationDemo/SnapNavigationDemo/SnapNavigationDemoApp.swift
+++ SnapNavigationDemo/SnapNavigationDemo/SnapNavigationDemoApp.swift
@@ -3,8 +3,8 @@
 //  Created by Simon Nickel
 //
 
-import SwiftUI
 import SnapNavigation
+import SwiftUI
 
 @Observable class AppState {
 	var navigationStyle: SnapNavigation.NavigationStyle = .tabsAdaptable
